0177ab204dfa500e5a75aaad5ed8e4f673fa5137
Siddharth Mondal <siddharthmandal32@gmail.com>
2023-10-01 20:24:53 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<h1> To do list </h1>
+
+<h2>IEEE</h2>
+
+<style>
+    h1 {color:red;}
+    h2 {color: blue;}
+  </style>
+
+<script>
+    let todos = [];
+    function addTodo() {
+        todos = [...todos,''];
+    }
+    function removeSelf(indeX) {
+        todos = [...todos.slice(0, indeX), ...todos.slice(index+1)];
+    }
+</script>
+{#each todos as todo, index}
+<input bind:value={todos[index]}>
+<button on:click={()=>removeSelf(index)}>X</button>
+<br />
+
+{/each}
+<button on:click={addTodo}>Add</button>
