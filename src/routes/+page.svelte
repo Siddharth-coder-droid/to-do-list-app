34a2f788d709d53eb7f38f040a90ec32db570d20
Siddharth Mondal <siddharthmandal32@gmail.com>
2023-10-21 19:28:48 +0530

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,41 @@
 <!DOCTYPE html>
-<!-- svelte-ignore a11y-missing-attribute -->
-<h1>HTML MINIFY</h1>
-<!-- svelte-ignore a11y-missing-attribute -->
-<html>
-    <head>
-     <!--<link rel="stylesheet" type="text/css" href="style.css">-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XML Indentation</title>
+    <style>
+        textarea { width: 100%; height: 150px; margin-bottom: 10px; }
+        button { margin-bottom: 20px; }
+    </style>
 </head>
 <body>
-    <!-- svelte-ignore a11y-missing-content -->
-    <a href="C:\Users\Admin\myapp\src\routes\app.js"></a>
-    <div id="formatter-container">
-        <textarea class="large-area--input" placeholder="Enter HTML code here"></textarea>
-        <br>
-        <!--<button class="controls__button--format">Format HTML</button>-->
-        <button class="controls__button--minify">Minify HTML</button>
-        <br>
-        <textarea class="large-area--output"></textarea>
+    <h1>XML Indentation</h1>
 
-    </div>
-    <script src="formatter.js"></script>
+    <label for="xmlInput">XML Input:</label>
+    <textarea id="xmlInput"></textarea>
+
+    <label for="indentation">Select Indentation:</label>
+    <select id="indentation"><option>2</option><option>4</option><option>8</option></select>
+
+    <button id="indentButton">Indent XML</button>
+
+    <h2>Indented XML Output:</h2>
+    <textarea id="xmlOutput" readonly></textarea>
 
     <script>
-        const inputArea = document.querySelector(".large-area--input");
-    const outputArea = document.querySelector(".large-area--output");
-    //const btnFormat = document.querySelector(".controls__button--format");
-    const btnMinify = document.querySelector(".controls__button--minify");
-    
-    /*btnFormat.addEventListener("click", () => {
-         const formattedHTML = formatHTML(inputArea.value);
-         outputArea.value = formattedHTML;
-         });*/
-    
-         btnMinify.addEventListener("click", () => {
-            const minifiedHTML = minifyHTML(inputArea.value);
-            outputArea.value = minifiedHTML;
-        });
-    
-        /*function formatHTML(htmlString) {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(htmlString, "text/html");
-            const formattedHTML = new XMLSerializer().serializeToString(xmlDoc);
-            return formattedHTML;
-            }*/
-    
-            function minifyHTML(htmlString) {
-            return htmlString.replace(/>\s+</g, '><').trim();
-        }
-    </script>
+        const xmlInput = document.getElementById("xmlInput");
+        const indentation = document.getElementById("indentation");
+        const xmlOutput = document.getElementById("xmlOutput");
+        const indentButton = document.getElementById("indentButton");
 
+        function indentXml() {
+            const spaces = " ".repeat(parseInt(indentation.value));
+            const indentedXml = xmlInput.value.replace(/\n/g, `\n${spaces}`);
+            xmlOutput.value = indentedXml;
+        }
+
+        indentButton.addEventListener("click", indentXml);
+    </script>
 </body>
 </html>
